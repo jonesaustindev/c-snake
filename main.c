40755bef59a7689765db137582e7b22bfaf0b2ac
Austin Jones <jonesaustindev@gmail.com>
2024-05-23 00:03:47 -0500

    add snake grow, game reset, etc

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 //
 // Data
 //
-#define GRID_ROWS 50
-#define GRID_COLUMNS 50
-#define GRID_SIZE 20
+#define GRID_ROWS 20
+#define GRID_COLUMNS 20
+#define GRID_SIZE 30
+#define DEFAULT_SPEED_INCREMENT 0.125
+#define SPEED_INCREMENT 0.005
 
 Color background_color = {71, 49, 68, 255};
-int screen_width = 1280;
-int screen_height = 720;
+int screen_width = 800;
+int screen_height = 800;
 
 int grid_width = GRID_COLUMNS * GRID_SIZE;
 int grid_height = GRID_ROWS * GRID_SIZE;
@@ -21,28 +23,33 @@ int grid_height = GRID_ROWS * GRID_SIZE;
 //
 // Objects
 //
-typedef enum Direction { LEFT, RIGHT, UP, DOWN } Direction;
+typedef enum { LEFT, RIGHT, UP, DOWN } Direction;
 
-typedef struct Player {
-  Vector2 position;
-  float speed;
-  int snake_length;
+typedef struct {
+  Vector2 current;
+  Vector2 last;
+} Position;
+
+typedef struct {
+  Position position;
   Direction direction;
 } Player;
 
-typedef struct InputManager {
+typedef struct {
   bool left;
   bool right;
   bool up;
   bool down;
+  bool enter;
 } InputManager;
 
-typedef enum Scene { MENU, PLAY, PAUSE, GAME_OVER } Scene;
+typedef enum { MENU, PLAY, PAUSE, GAME_OVER } Scene;
 
-typedef struct GameState {
+typedef struct {
   Player *player;
+  Position body[100];
+  int body_length;
   Scene current_scene;
-  int grid[GRID_ROWS][GRID_COLUMNS];
   struct {
     Vector2 position;
     bool is_alive;
@@ -62,20 +69,28 @@ Vector2 get_start_pos() {
   return start_pos;
 }
 
-Vector2 get_random_pos() {
-  int x = rand() % GRID_COLUMNS;
-  int y = rand() % GRID_ROWS;
+bool is_outside_grid(Vector2 position) {
+  Vector2 start_pos = get_start_pos();
+  int end_x = start_pos.x + grid_width;
+  int end_y = start_pos.y + grid_height;
 
-  // ugly but my math isn't great right now
-  if (x < 0) {
-    x *= -1;
+  if (position.x < start_pos.x || position.x >= end_x ||
+      position.y < start_pos.y || position.y >= end_y) {
+    return true;
+  } else {
+    return false;
   }
-  if (y < 0) {
-    y *= -1;
+}
+
+bool is_spawn_on_body(Vector2 position, GameState *state) {
+  for (int i = 0; i < state->body_length; i++) {
+    if (position.x == state->body[i].current.x &&
+        position.y == state->body[i].current.y) {
+      return true;
+    }
   }
 
-  Vector2 random_pos = {.x = x * GRID_SIZE, .y = y * GRID_SIZE};
-  return random_pos;
+  return false;
 }
 
 void reset_input(InputManager *input) {
@@ -83,6 +98,37 @@ void reset_input(InputManager *input) {
   input->down = false;
   input->left = false;
   input->right = false;
+  input->enter = false;
+}
+
+Vector2 get_random_pos(GameState *state) {
+  Vector2 start_pos = get_start_pos();
+  Vector2 random_pos;
+
+  do {
+    int x = rand() % GRID_COLUMNS;
+    int y = rand() % GRID_ROWS;
+
+    random_pos.x = start_pos.x + x * GRID_SIZE;
+    random_pos.y = start_pos.y + y * GRID_SIZE;
+  } while (is_outside_grid(random_pos) && is_spawn_on_body(random_pos, state));
+
+  return random_pos;
+}
+
+Player *setup_player() {
+  Player *player = (Player *)malloc(sizeof(Player));
+
+  if (player == NULL) {
+    printf("player could not be allocated\n");
+    return 0;
+  }
+
+  player->position.current.x = (float)GRID_COLUMNS / 2;
+  player->position.current.y = (float)GRID_ROWS / 2;
+  player->direction = RIGHT;
+
+  return player;
 }
 
 //
@@ -93,43 +139,100 @@ void handle_input(InputManager *input) {
   input->right = IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D);
   input->up = IsKeyDown(KEY_UP) || IsKeyDown(KEY_W);
   input->down = IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S);
+  input->enter = IsKeyDown(KEY_ENTER);
+}
+
+void hit_detection(GameState *state) {
+  Vector2 pixel_position = {
+      get_start_pos().x + state->player->position.current.x * GRID_SIZE,
+      get_start_pos().y + state->player->position.current.y * GRID_SIZE};
+
+  // snake ate the food
+  // set to not alive to be respawned
+  // speed up movement
+  if (pixel_position.x == state->food.position.x &&
+      pixel_position.y == state->food.position.y) {
+    state->food.is_alive = false;
+    state->body_length += 1;
+
+    if (state->body_length > 0) {
+      state->body[state->body_length - 1] = state->player->position;
+    } else {
+      state->body[state->body_length - 1] = state->body[state->body_length - 2];
+    }
+
+    state->move_rate -= SPEED_INCREMENT;
+  }
 }
 
 void update(GameState *state, InputManager *input) {
-  if (input->left && state->player->direction != RIGHT) {
-    state->player->direction = LEFT;
-  } else if (input->right && state->player->direction != LEFT) {
-    state->player->direction = RIGHT;
-  } else if (input->up && state->player->direction != DOWN) {
-    state->player->direction = UP;
-  } else if (input->down && state->player->direction != UP) {
-    state->player->direction = DOWN;
+  if (state->current_scene == PLAY) {
+    state->player->position.last = state->player->position.current;
+
+    if (input->left && state->player->direction != RIGHT) {
+      state->player->direction = LEFT;
+    } else if (input->right && state->player->direction != LEFT) {
+      state->player->direction = RIGHT;
+    } else if (input->up && state->player->direction != DOWN) {
+      state->player->direction = UP;
+    } else if (input->down && state->player->direction != UP) {
+      state->player->direction = DOWN;
+    }
+
+    static float accumulator = 0;
+    accumulator += GetFrameTime();
+
+    if (accumulator >= state->move_rate) {
+      switch (state->player->direction) {
+      case LEFT:
+        if (state->player->position.current.x - 1 < GRID_ROWS) {
+        }
+        state->player->position.current.x -= 1;
+        break;
+      case RIGHT:
+        state->player->position.current.x += 1;
+        break;
+      case UP:
+        state->player->position.current.y -= 1;
+        break;
+      case DOWN:
+        state->player->position.current.y += 1;
+        break;
+      }
+
+      // snake body
+      for (int i = 0; i < state->body_length; i++) {
+        if (i == 0) {
+          state->body[i].last = state->body[i].current;
+          state->body[i].current = state->player->position.last;
+        } else {
+          state->body[i].last = state->body[i].current;
+          state->body[i].current = state->body[i - 1].last;
+        }
+      }
+
+      accumulator = 0;
+    }
+
+    hit_detection(state);
   }
 
-  static float accumulator = 0;
-  accumulator += GetFrameTime();
+  if (state->current_scene == GAME_OVER) {
+    if (input->enter) {
+      state->current_scene = PLAY;
 
-  if (accumulator >= state->move_rate) {
-    switch (state->player->direction) {
-    case LEFT:
-      state->player->position.x -= 1;
-      break;
-    case RIGHT:
-      state->player->position.x += 1;
-      break;
-    case UP:
-      state->player->position.y -= 1;
-      break;
-    case DOWN:
-      state->player->position.y += 1;
-      break;
+      free(state->player);
+      state->player = setup_player();
+
+      state->food.is_alive = false;
+      state->body_length = 0;
+      state->move_rate = DEFAULT_SPEED_INCREMENT;
     }
-    accumulator = 0;
   }
 }
 
 //
-// Render
+// Draw
 //
 void draw_debug() {
   char fps_str[32];
@@ -137,7 +240,36 @@ void draw_debug() {
   DrawText(fps_str, 10, 10, 20, LIGHTGRAY);
 }
 
-void render_grid(GameState *state) {
+void draw_game_over() {
+  static char game_over_str[32];
+  static char subtitle_str[64];
+  sprintf(game_over_str, "Game Over");
+  sprintf(subtitle_str, "Press 'Enter' to play again");
+
+  int font_size = 72;
+  int subtitle_font_size = 24;
+
+  int game_over_width = MeasureText(game_over_str, font_size);
+  int game_over_height = font_size;
+
+  int subtitle_width = MeasureText(subtitle_str, subtitle_font_size);
+  int subtitle_height = subtitle_font_size;
+
+  Vector2 game_over_position;
+  game_over_position.x = (float)(screen_width - game_over_width) / 2;
+  game_over_position.y = (float)(screen_height - game_over_height) / 2;
+
+  Vector2 subtitle_position;
+  subtitle_position.x = (float)(screen_width - subtitle_width) / 2;
+  subtitle_position.y = game_over_position.y + game_over_height + 20;
+
+  DrawText(game_over_str, game_over_position.x, game_over_position.y, font_size,
+           LIGHTGRAY);
+  DrawText(subtitle_str, subtitle_position.x, subtitle_position.y,
+           subtitle_font_size, LIGHTGRAY);
+}
+
+void draw_grid(GameState *state) {
   int grid_width = GRID_COLUMNS * GRID_SIZE;
   int grid_height = GRID_ROWS * GRID_SIZE;
 
@@ -155,52 +287,63 @@ void render_grid(GameState *state) {
   }
 }
 
-void render_food(GameState *state) {
+void draw_food(GameState *state) {
   Vector2 size = {.x = GRID_SIZE, .y = GRID_SIZE};
 
   if (!state->food.is_alive) {
-    Vector2 pos = get_random_pos();
-    Vector2 start_pos = get_start_pos();
-    state->food.position.x = start_pos.x + pos.x;
-    state->food.position.y = start_pos.y + pos.y;
+    state->food.position = get_random_pos(state);
     state->food.is_alive = true;
   }
 
-  DrawRectangleV(state->food.position, size, WHITE);
+  DrawRectangleV(state->food.position, size, BLUE);
 }
 
-void render(GameState *state) {
-  // grid
-  render_grid(state);
+void draw(GameState *state) {
+  draw_grid(state);
 
-  // food
-  render_food(state);
+  draw_food(state);
 
-  // player
-  Vector2 size = {.x = GRID_SIZE, .y = GRID_SIZE};
-  Vector2 start_pos = get_start_pos();
-  Vector2 pixel_position = {start_pos.x + state->player->position.x * GRID_SIZE,
-                            start_pos.y +
-                                state->player->position.y * GRID_SIZE};
-  DrawRectangleV(pixel_position, size, WHITE);
+  if (state->current_scene == PLAY) {
+    // player
+    Vector2 size = {.x = GRID_SIZE, .y = GRID_SIZE};
+    Vector2 start_pos = get_start_pos();
+    Vector2 player_position = {
+        start_pos.x + state->player->position.current.x * GRID_SIZE,
+        start_pos.y + state->player->position.current.y * GRID_SIZE};
+
+    if (is_outside_grid(player_position)) {
+      state->current_scene = GAME_OVER;
+    }
+
+    DrawRectangleV(player_position, size, WHITE);
+
+    // snake body
+    for (int i = 0; i < state->body_length; i++) {
+      Vector2 size = {.x = GRID_SIZE, .y = GRID_SIZE};
+      Vector2 start_pos = get_start_pos();
+      Vector2 pixel_position = {
+          start_pos.x + state->body[i].current.x * GRID_SIZE,
+          start_pos.y + state->body[i].current.y * GRID_SIZE};
+      DrawRectangleV(pixel_position, size, WHITE);
+    }
+  }
+
+  if (state->current_scene == GAME_OVER) {
+    draw_game_over();
+  }
 }
 
 //
 // Main
 //
 int main(void) {
-  srand(time(0));
-  Vector2 start_pos = get_start_pos();
-  Player player = {.position.x = (float)GRID_COLUMNS / 2,
-                   .position.y = (float)GRID_ROWS / 2,
-                   .speed = 10.0f,
-                   .snake_length = 1,
-                   .direction = RIGHT};
+  srand(time(NULL));
 
-  GameState game_state = {.player = &player,
+  GameState game_state = {.player = setup_player(),
+                          .body_length = 0,
                           .current_scene = PLAY,
                           .food = {.is_alive = false},
-                          .move_rate = 0.1};
+                          .move_rate = 0.125};
 
   InputManager input_manager = {
       .left = false, .right = false, .up = false, .down = false};
@@ -220,10 +363,13 @@ int main(void) {
     handle_input(&input_manager);
 
     update(&game_state, &input_manager);
-    render(&game_state);
+    draw(&game_state);
 
     EndDrawing();
   }
+
+  // memory cleanup
+  free(game_state.player);
 
   CloseWindow();
 
